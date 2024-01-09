08c0c84769399f25928702907a8c74ce6aaf5c6f
Rahul Ghag <ghagrahul96@gmail.com>
2024-01-09 23:02:58 +0530

    Refactor

--- app/src/main/java/com/rahulghag/blogapp/data/remote/mappers/UserDomainMapper.kt
+++ app/src/main/java/com/rahulghag/blogapp/data/remote/mappers/UserDomainMapper.kt
@@ -5,13 +5,13 @@ import com.rahulghag.blogapp.data.utils.DomainMapper
 import com.rahulghag.blogapp.domain.models.User
 
 class UserDomainMapper : DomainMapper<UserDto, User> {
-    override fun mapToDomainModel(model: UserDto): User {
+    override fun mapToDomainModel(data: UserDto): User {
         return User(
-            bio = model.bio,
-            email = model.email,
-            image = model.image,
-            token = model.token,
-            username = model.username
+            bio = data.bio,
+            email = data.email,
+            image = data.image,
+            token = data.token,
+            username = data.username
         )
     }
 }
--- app/src/main/java/com/rahulghag/blogapp/data/utils/EntityMapper.kt
+++ app/src/main/java/com/rahulghag/blogapp/data/utils/EntityMapper.kt
@@ -1,5 +1,5 @@
 package com.rahulghag.blogapp.data.utils
 
 interface DomainMapper<T, DomainModel> {
-    fun mapToDomainModel(model: T): DomainModel
+    fun mapToDomainModel(data: T): DomainModel
 }
